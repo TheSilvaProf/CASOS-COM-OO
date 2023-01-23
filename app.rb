10bcfa8cc1876900a043a60a2f83562f6bbd0e4e
TheSilvaProf <fabio01@protonmail.ch>
2023-01-23 07:34:54 -0500

    Reolvido Erro de utf8

--- app.rb
+++ app.rb
@@ -3,9 +3,11 @@ require "csv"
 puts "Processador de Arquivos CSV"
 
 file = File.read("./data/atelie-catalogo-produtos.csv")
-
-csv = CSV.parse{ |io| io.read.encode("UTF-8", invalid: :replace, replace: "") }
-csv.each do |row|
-    puts row
+file.force_encoding(file).scrub!("")
+csv = CSV.parse(file, headers: true)
+file.each do |row|
+    puts row.class
 end
 #open("file.txt", "r") { |io| io.read.encode("UTF-8", invalid: :replace, replace: "") }
+
+#CSV.new('').encoding # => #<Encoding:UTF-8>
